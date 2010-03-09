a2509a88664f145bde267a9fd1b634fb0ee6c4b7
Marcin Magnus <mag_dex@o2.pl>
2010-03-09 16:59:08 +0100

    fix - readme, more doc strings

--- pubmex.py
+++ pubmex.py
@@ -59,7 +59,7 @@ def get_title_via_pmid(pmid,v=0):
 {'DOI': '10.1261/rna.283207', 'Title': 'Conversion of pre-RISC to holo-RISC by Ago2 during assembly of RNAi complexes.', 'Source': 'RNA', 'PmcRefCount': 9, 'Issue': '1', 'SO': '2007 Jan;13(1):22-9', 'ISSN': '1355-8382', 'Volume': '13', 'FullJournalName': 'RNA (New York, N.Y.)', 'RecordStatus': 'PubMed - indexed for MEDLINE', 'ESSN': '1469-9001', 'ELocationID': '', 'Pages': '22-9', 'PubStatus': 'ppublish+epublish', 'AuthorList': ['Kim K', 'Lee YS', 'Carthew RW'], 'EPubDate': '2006 Nov 22', 'PubDate': '2007 Jan', 'NlmUniqueID': '9509184', 'LastAuthor': 'Carthew RW', 'ArticleIds': {'pii': 'rna.283207', 'medline': [], 'pubmed': ['17123955'], 'pmc': 'PMC1705758', 'pmcid': 'pmc-id: PMC1705758;', 'doi': '10.1261/rna.283207'}, u'Item': [], 'History': {'medline': ['2007/01/31 09:00'], 'pubmed': ['2006/11/25 09:00'], 'entrez': '2006/11/25 09:00', 'aheadofprint': '2006/11/22 00:00'}, 'LangList': ['English'], 'HasAbstract': 1, 'References': [], 'PubTypeList': ['Journal Article'], u'Id': '17123955'}
 
     RETURN:
-    - formated title(string) KIM_K.CARTHEW_RW-Conversion.of.pre-RISC.to.holo-RISC.by.Ago2.during.assembly.of.RNAi.complexes.17123955.2007.RNA.pdf 
+    - formated title(string) KIM_K.CARTHEW_RW-Conversion.of.pre-RISC.to.holo-RISC.by.Ago2.during.assembly.of.RNAi.complexes.17123955..RNA.2007.pdf  !!! problem !!
     !!
     need xclip - http://sourceforge.net/projects/xclip/
     """
@@ -70,7 +70,7 @@ def get_title_via_pmid(pmid,v=0):
     if v:print summary_dict
 
     titleOfpub=dotting(summary_dict['Title'].strip())
-    title=upper(underline(summary_dict['AuthorList'][0].strip())+'.'+underline(summary_dict['AuthorList'][-1].strip()))+'-'+titleOfpub+'.'+pmid+'.'+str(summary_dict['PubDate'].split()[0])+'.'+upper(dotting(summary_dict['Source']))+'.pdf'
+    title=upper(underline(summary_dict['AuthorList'][0].strip())+'.'+underline(summary_dict['AuthorList'][-1].strip()))+'-'+titleOfpub+'.'+pmid+'.'+upper(dotting(summary_dict['Source']))+'.'+str(summary_dict['PubDate'].split()[0])+'.pdf'
     title=clean_string(title)
     cmd='echo '+title + ' | xclip -selection clipboard'
     if v:print cmd
@@ -98,10 +98,12 @@ def get_title_via_doi(doi,v=0):
     return get_title_via_pmid(idlist[0])
 
 def get_options():
-    #
+    """
+    get options
+    """
     usage="%prog [options] id"
     description="""
-examples: pubmex.py -d  17123955, pumex.py -d 10.1038/embor.2008.212
+examples: pubmex.py -p 17123955, pumex.py -d 10.1038/embor.2008.212
 """
     version=VERSION
     #
@@ -119,7 +121,6 @@ examples: pubmex.py -d  17123955, pumex.py -d 10.1038/embor.2008.212
                 help="pass DOI of the paper",metavar="DOI_ID",default='')
     #
     (options, arguments) = p.parse_args()
-    ##print     (options, arguments)
     ##(<Values at 0xb7aab50c: {'doi': '', 'pubmed_id': True}>, ['17123955'])
     ##{'doi': '', 'pubmed_id': True} ['17123955']
     if len(arguments) > 0:

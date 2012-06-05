6dae0df19d34894ad2c1845f3c042d2d6c1f8c37
magnus <magnus@netbook.(none)>
2012-06-05 22:28:25 +0200

    Package! works :-)) ----------------------- 1. pubmex 2. README
    magnus@netbook:/usr/local/lib/python2.6/dist-packages/pubmex-0.3a-py2.6.egg$ ls -l
    total 40
    drwxr-sr-x 2 root staff  4096 Jun  5 21:55 demo
    drwxr-sr-x 2 root staff  4096 Jun  5 21:55 EGG-INFO
    -rwxr-xr-x 1 root staff 13235 Jun  5 21:55 pubmex.py
    -rw-r--r-- 1 root staff 12085 Jun  5 21:55 pubmex.pyc
    -rw-r--r-- 1 root staff  1293 Jun  5 21:55 README

---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+
+
+def get_version():
+    f = open('pubmex.py')
+    try:
+        for line in f:
+            if line.startswith('__version__'):
+                return eval(line.split('=')[-1])
+    finally:
+        f.close()
+
+
+def get_long_description():
+    descr = []
+    for fname in ['README']:  # , # 'CHANGES.txt':   # , 'TODO.txt'
+        f = open(fname)
+        try:
+            descr.append(f.read())
+        finally:
+            f.close()
+    return '\n\n'.join(descr)
+
+
+setup(
+    name='pubmex',
+    version=get_version(),
+    description="pu(b)mex: a scientific publication renamer",
+    long_description=get_long_description(),
+    keywords='publication, pubmed, doi, pmid',
+    author='Marcin Magnus',
+    author_email='m.magnus@o2.pl',
+    url='https://github.com/m4rx9/pubmex/wiki',
+    license='GPLv3',
+    py_modules=['pubmex'],
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        #'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    data_files=[('', ['README']),
+                ('demo', ['demo/demo01.pdf', 'demo/demo02.pdf'])
+                ],
+    entry_points={
+        'console_scripts': [
+            'pubmex = pubmex:main',
+        ],
+    },
+)

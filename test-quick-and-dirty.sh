85fbd8921e80b9f25790e8d8244c698790cc702d
magnus@maximus <magnus@genesilico.pl>
2012-06-18 16:18:39 +0200

    Add simple rude test bash script.
    ----------------------------------
    magnus@maximus:~/Dropbox/workspace/pubmexproject/pubmex$ ./test.sh
    # get a title based on PMID
    python pubmex.py -p 20533529
    DEBUG - temp generated
    [..]
    # get data based on a given pdf file, show a new new, DO rename
    pubmex.py -a -f demo/demo01.pdf -r
    DEBUG - temp generated
    {'keywords': None, 'rename': True, 'automatic': True, 'PMID_or_DOI': None, 'filename': 'demo/demo01.pdf'}
    FILENAME:	demo/demo01.pdf
    temp is going to be opened
    10.1016/J.CEB.2009.01.022
    IdList ['19223165']
    PMID: 19223165
    TITLE: 		Russel.Sali-The.structural.dynamics.macromolecular.processes-CurrOpinCellBiol.2009.pdf
    RENAME:		demo/demo01.pdf -*DO*-> demo/done/Russel.Sali-The.structural.dynamics.macromolecular.processes-CurrOpinCellBiol.2009.pdf
    
    revert the demo folder ...
    
    `demo/done/Russel.Sali-The.structural.dynamics.macromolecular.processes-CurrOpinCellBiol.2009.pdf' -> `demo/demo01.pdf'

---
+++ test-quick-and-dirty.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo '# get a title based on PMID'
+echo 'python pubmex.py -p 20533529 '
+python pubmex.py -p 20533529 
+echo
+echo '# get data based on PMID, change a title to keywords'
+echo \'python pubmex.py -p 20533529 -k 'MLH1,PMS2'\'
+python pubmex.py -p 20533529 -k 'MLH1,PMS2'
+echo
+echo '# get data based on DOI'
+echo 'python pubmex.py -p 10.1038/embor.2008.212'
+python pubmex.py -p 10.1038/embor.2008.212
+echo
+echo '# get data based on a given pdf file, show a new new, DO NOT rename'
+echo 'python pubmex.py -a -f demo/demo01.pdf'
+python pubmex.py -a -f demo/demo01.pdf
+echo
+echo '# get data based on a given pdf file, show a new new, DO rename'
+echo 'pubmex.py -a -f demo/demo01.pdf -r'
+python pubmex.py -a -f demo/demo01.pdf -r
+echo 
+echo 'revert the demo folder ...'
+echo 
+mv -v demo/done/Russel.Sali-The.structural.dynamics.macromolecular.processes-CurrOpinCellBiol.2009.pdf demo/demo01.pdf 

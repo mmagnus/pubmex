2463a9690e4c89a23e765f5940c2f26ae5e3ad0d
magnus <magnus@netbook.(none)>
2012-07-03 09:42:27 +0200

    Rename examples to examples-test. Remove test-qu*.sh
    ----------------------------------
    It was not consisted with DRY rule. The code from examples was
    duplicated in test-qu*.sh.
    
    To test app run examples-test.sh
    
    magnus@netbook:~/Dropbox/workspace/pubmexproject/pubmex$ ./examples-test.sh
    # get a title based on PMID
    python pubmex.py -p 20533529
    Kosinski.Plotz.Identification.Lynch.syndrome.mutations.MLH1-PMS2.interface.that.disturb.dimerization.mismatch.repair.HumMutat.2010.pdf
    
    # get data based on PMID, change a title to keywords
    'python pubmex.py -p 20533529 -k MLH1,PMS2'
    Kosinski.Plotz.MLH1.PMS2.HumMutat.2010.pdf
    
    # get data based on PMID, change a title to keywords + debug info
    'python pubmex.py -d -p 20533529 -k MLH1,PMS2'
    debug:.............. on
    options: ........... {'rename': False, 'filename': None, 'debug': True, 'PMID_or_DOI': '20533529', 'keywords': 'MLH1,PMS2', 'automatic': None}
    arguments: ......... []
    summary_dict........ {'DOI': '10.1002/humu.21301', 'Title': 'Identification of Lynch syndrome mutations in the MLH1-PMS2 interface that disturb dimerization and mismatch repair.', 'Source': 'Hum Mutat', 'PmcRefCount': 1, 'Issue': '8', 'SO': '2010 Aug;31(8):975-82', 'ISSN': '1059-7794', 'Volume': '31', 'FullJournalName': 'Human mutation', 'RecordStatus': 'PubMed - indexed for MEDLINE', 'ESSN': '1098-1004', 'ELocationID': '', 'Pages': '975-82', 'PubStatus': 'ppublish', 'AuthorList': ['Kosinski J', 'Hinrichsen I', 'Bujnicki JM', 'Friedhoff P', 'Plotz G'], 'EPubDate': '', 'PubDate': '2010 Aug', 'NlmUniqueID': '9215429', 'LastAuthor': 'Plotz G', 'ArticleIds': {'medline': [], 'mid': 'NIHMS212166', 'pubmed': ['20533529'], 'eid': '20533529', 'pmc': 'PMC2908215', 'rid': '20533529', 'pmcid': 'pmc-id: PMC2908215;manuscript-id: NIHMS212166;', 'doi': '10.1002/humu.21301'}, u'Item': [], 'History': {'medline': ['2010/10/21 06:00'], 'pubmed': ['2010/06/10 06:00'], 'entrez': '2010/06/10 06:00'}, 'LangList': ['English'], 'HasAbstract': 1, 'References': [], 'PubTypeList': ['Journal Article'], u'Id': '20533529'}
    Kosinski.Plotz.MLH1.PMS2.HumMutat.2010.pdf
    
    # get data based on DOI
    python pubmex.py -p 10.1038/embor.2008.212
    Kelley.Scott.The.evolution.biology.shift.towards.engineering.prediction-generating.tools.away.traditional.research.practice.EMBORep.2008.pdf
    
    # get data based on a given pdf file, show a new new, DO NOT rename
    python pubmex.py -a -f demo/demo01.pdf
    the title is ...  Russel.Sali.The.structural.dynamics.macromolecular.processes.CurrOpinCellBiol.2009.pdf
    CAUTION! THE FILE WAS NOT RENAME, ADD -r OPTION TO RENAME THE FILE. IF YOU USE -r THE PUBMEX WILL DO
    mv demo/demo01.pdf --> demo/done/Russel.Sali.The.structural.dynamics.macromolecular.processes.CurrOpinCellBiol.2009.pdf
    
    # get data based on a given pdf file, show a new new, DO rename
    pubmex.py -a -f demo/demo01.pdf -r
    the title is ...  Russel.Sali.The.structural.dynamics.macromolecular.processes.CurrOpinCellBiol.2009.pdf
    mv  demo/demo01.pdf --> demo/done/Russel.Sali.The.structural.dynamics.macromolecular.processes.CurrOpinCellBiol.2009.pdf

--- examples-test.sh
+++ examples-test.sh
@@ -6,6 +6,10 @@ echo '# get data based on PMID, change a title to keywords'
 echo \'python pubmex.py -p 20533529 -k 'MLH1,PMS2'\'
 python pubmex.py -p 20533529 -k 'MLH1,PMS2'
 echo
+echo '# get data based on PMID, change a title to keywords + debug info'
+echo \'python pubmex.py -d -p 20533529 -k 'MLH1,PMS2'\'
+python pubmex.py -d -p 20533529 -k 'MLH1,PMS2'
+echo
 echo '# get data based on DOI'
 echo 'python pubmex.py -p 10.1038/embor.2008.212'
 python pubmex.py -p 10.1038/embor.2008.212
